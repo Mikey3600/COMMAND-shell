6e01b63daffd3a6e2514c0123fd421f520e652f0
MAYANK <24052419>
2025-12-06 07:10:47 +0530

    stage3: implemented REPL loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,20 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-   std::cout << "$ ";
-       std::string input;
-    std::getline(std::cin, input);  // read command
+    while (true) {
+        std::cout << "$ ";                 // Print prompt
 
-    std::cout << input << ": command not found" << std::endl;
+        std::string input;
+        if (!std::getline(std::cin, input)) {
+            break; // If input stream closes, exit loop (tester controls this)
+        }
+
+        std::cout << input << ": command not found" << std::endl;
+    }
+
     return 0;
 }
-
